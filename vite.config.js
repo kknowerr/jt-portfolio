2faa31115caec0f89f1952222e64b13cf29cef9f
Jacob Trevino <91908115+l0sttt@users.noreply.github.com>
2023-06-28 03:09:56 -0700

    fixing build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/jt-portfolio/' : './',
+  build: {
+    outDir: 'dist',
+    assetsDir: './',
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
 })
